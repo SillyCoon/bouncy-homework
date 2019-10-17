d5a4d16e718f6014d6211c0b97cdb8b24e28e639
Aleksey Ivanov <aleksey.ivanov@arcadia.spb.ru>
2019-10-17 15:17:56 +0300

    add base encrypt-decrypt functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,75 @@
+import org.bouncycastle.cms.*;
+import org.bouncycastle.cms.jcajce.JceCMSContentEncryptorBuilder;
+import org.bouncycastle.cms.jcajce.JceKeyTransEnvelopedRecipient;
+import org.bouncycastle.cms.jcajce.JceKeyTransRecipient;
+import org.bouncycastle.cms.jcajce.JceKeyTransRecipientInfoGenerator;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.operator.OutputEncryptor;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Collection;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CertificateException, NoSuchProviderException, IOException, KeyStoreException, UnrecoverableKeyException, NoSuchAlgorithmException {
+        Security.addProvider(new BouncyCastleProvider());
+        CertificateFactory certFactory = CertificateFactory
+                .getInstance("X.509", "BC");
+        X509Certificate certificate = (X509Certificate) certFactory
+                .generateCertificate(new FileInputStream("src/main/resources/public.cer"));
+        char[] keystorePassword = "password".toCharArray();
+        char[] keyPassword = "password".toCharArray();
+        KeyStore keystore = KeyStore.getInstance("PKCS12");
+        keystore.load(new FileInputStream("src/main/resources/private.p12"), keystorePassword);
+        PrivateKey privateKey = (PrivateKey) keystore.getKey("baeldung",
+                keyPassword);
 
     }
+
+    public static byte[] encryptData(byte[] data,
+                                     X509Certificate encryptionCertificate)
+            throws CertificateEncodingException, CMSException, IOException {
+        byte[] encryptedData = null;
+        if (null != data && null != encryptionCertificate) {
+            CMSEnvelopedDataGenerator cmsEnvelopedDataGenerator
+                    = new CMSEnvelopedDataGenerator();
+            JceKeyTransRecipientInfoGenerator jceKey
+                    = new JceKeyTransRecipientInfoGenerator(encryptionCertificate);
+            cmsEnvelopedDataGenerator.addRecipientInfoGenerator(jceKey);
+            CMSTypedData msg = new CMSProcessableByteArray(data);
+            OutputEncryptor encryptor
+                    = new JceCMSContentEncryptorBuilder(CMSAlgorithm.AES128_CBC)
+                    .setProvider("BC").build();
+            CMSEnvelopedData cmsEnvelopedData = cmsEnvelopedDataGenerator
+                    .generate(msg,encryptor);
+            encryptedData = cmsEnvelopedData.getEncoded();
+        }
+        return encryptedData;
+    }
+
+    public static byte[] decryptData(
+            byte[] encryptedData,
+            PrivateKey decryptionKey)
+            throws CMSException {
+        byte[] decryptedData = null;
+        if (null != encryptedData && null != decryptionKey) {
+            CMSEnvelopedData envelopedData = new CMSEnvelopedData(encryptedData);
+            Collection<RecipientInformation> recipients
+                    = envelopedData.getRecipientInfos().getRecipients();
+            KeyTransRecipientInformation recipientInfo
+                    = (KeyTransRecipientInformation) recipients.iterator().next();
+            JceKeyTransRecipient recipient
+                    = new JceKeyTransEnvelopedRecipient(decryptionKey);
+            return recipientInfo.getContent(recipient);
+        }
+        return decryptedData;
+    }
+
 }
